ee766ce3e3ec503e29f327c63e93b79f5f62b533
Ambati Sahithi <162500856+asr2003@users.noreply.github.com>
2024-10-25 23:35:15 +0530

    add test

--- metrics/opentelemetry-metrics/src/test/scala/sttp/tapir/server/metrics/opentelemetry/OpenTelemetryMetricsTest.scala
+++ metrics/opentelemetry-metrics/src/test/scala/sttp/tapir/server/metrics/opentelemetry/OpenTelemetryMetricsTest.scala
@@ -101,7 +101,7 @@ class OpenTelemetryMetricsTest extends AnyFlatSpec with Matchers {
               "GET",
               AttributeKey.stringKey("path"),
               "/person",
-              AttributeKey.longKey("http.response.status_code").asInstanceOf[AttributeKey[_ <: Any]],
+              AttributeKey.longKey("http.response.status_code"),
               200L
             ) && dp.getValue == 2 =>
           true
@@ -111,7 +111,7 @@ class OpenTelemetryMetricsTest extends AnyFlatSpec with Matchers {
               "GET",
               AttributeKey.stringKey("path"),
               "/person",
-              AttributeKey.longKey("http.response.status_code").asInstanceOf[AttributeKey[_ <: Any]],
+              AttributeKey.longKey("http.response.status_code"),
               400L
             ) && dp.getValue == 2 =>
           true
@@ -154,7 +154,7 @@ class OpenTelemetryMetricsTest extends AnyFlatSpec with Matchers {
         "GET",
         AttributeKey.stringKey("path"),
         "/person",
-        AttributeKey.longKey("http.response.status_code").asInstanceOf[AttributeKey[_ <: Any]],
+        AttributeKey.longKey("http.response.status_code"),
         200L
       )
     )
@@ -209,7 +209,7 @@ class OpenTelemetryMetricsTest extends AnyFlatSpec with Matchers {
       "GET",
       AttributeKey.stringKey("path"),
       "/person",
-      AttributeKey.longKey("http.response.status_code").asInstanceOf[AttributeKey[_ <: Any]],
+      AttributeKey.longKey("http.response.status_code"),
       500L
     )
     point.getValue shouldBe 1
