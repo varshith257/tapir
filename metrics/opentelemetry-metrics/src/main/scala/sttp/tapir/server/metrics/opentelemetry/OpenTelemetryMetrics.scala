6ccc04bdea2de8df7c80ba44a87ff1fa3f052a2b
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-11-13 23:36:56 +0530

    Update OpenTelemetryMetrics.scala

--- metrics/opentelemetry-metrics/src/main/scala/sttp/tapir/server/metrics/opentelemetry/OpenTelemetryMetrics.scala
+++ metrics/opentelemetry-metrics/src/main/scala/sttp/tapir/server/metrics/opentelemetry/OpenTelemetryMetrics.scala
@@ -59,7 +59,7 @@ object OpenTelemetryMetrics {
         case Left(ex) => Some(ex.getClass.getSimpleName)
         case Right(_) => None
       }
-    )
+    ).collect { case (k, v) if v.nonEmpty => k -> v }
   )
 
   def apply[F[_]](meter: Meter): OpenTelemetryMetrics[F] = apply(meter, Nil)
